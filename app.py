b4d328f2f79bf3c78c76d2a9c28b0d259cd7cf47
mattevans0911 <102312529+mattevans0911@users.noreply.github.com>
2022-07-12 22:33:13 -0400

    Added more end points to API

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class Book(db.Model): #inheriting from db class model
     review = db.Column(db.String, nullable=True) #Allows the user to now leave a review, since nullable is true 
     genre = db.Column(db.String, nullable=False) #Tells user they need to include genre, since nullable is false
 
-    def __init__(self,id, title, author, review, genre): #constructor class, this is how our books will look when created (blueprint), and they will follow the rules we set 
+    def __init__(self, title, author, review, genre): #constructor class, this is how our books will look when created (blueprint), and they will follow the rules we set 
         self.title = title #instance of title
         self.author = author #instance of author 
         self.review = review #instance of review
@@ -47,11 +47,42 @@ def add_book(): #happens everytime user sends a request, like a call back functi
 
     return jsonify('You have added a new book.') #tells the user they have added a new book 
 
-@app.route('/book/get', methods = ['GET']) #receive all the books added to databse
+@app.route('/book/get', methods=['GET']) #receive all the books added to databse
 def get_books():
     books = db.session.query(Book).all() #search database until we find what we are looking for
-    return jsonify(multiple_book_schema(books)) #using 
+    return jsonify(multiple_book_schema.dump(books)) #using .dump is a method that "dumps" our object into something we can use - The dump() method is used when the Python objects have to be stored in a file.
 
+
+@app.route('/book/update/<id>', methods=(['PUT', 'PATCH'])) #methods used for updating
+def update_book(id):
+    post_data = request.get_json() #broken down what user sends and stores into variables listed below
+    title = post_data.get('title')
+    author = post_data.get('author')
+    review = post_data.get('review')
+    genre = post_data.get('genre')
+
+    book = db.session.query(Book).filter(Book.id == id).first()
+    
+    if title != None:   #wrapped in an if statement to allow a "Safety Net" 
+        book.title = title  #if a user does not provide an update, we return the previous value
+    if author != None:
+        book.author = author
+    if review != None:
+        book.review = review
+    if genre != None:
+        book.genre = genre
+
+    db.session.commit()
+    return jsonify("Your book has been updated")
+
+
+@app.route('/book/delete/<id>', methods=['DELETE']) #reference instance of the book you want to remove, this is why we used <id>
+def remove_book(id): #pass the id into the function parameters
+    book = db.session.query(Book).filter(Book.id == id).first() #we have to find, or query, for the book we want to delete, then run a filter to filter out the book that matches the provided ID
+    db.session.delete(book) #deleting our instance of book
+    db.session.commit() #committing the changes
+
+    return jsonify(f'You have deleted book {id}')
 
 if __name__ == "__main__":  #call instance of our app 
     app.run(debug=True)
